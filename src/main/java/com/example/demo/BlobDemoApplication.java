0126430546795ec5ea252dfa43b1e409f307fbed
renatoFerraz-git <renatoferrazconta02@gmail.com>
2022-05-02 17:12:42 -0300

    word_to_pdf

--- src/main/java/com/example/demo/BlobDemoApplication.java
+++ src/main/java/com/example/demo/BlobDemoApplication.java
@@ -9,5 +9,4 @@ public class BlobDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlobDemoApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/demo/service/DocStorageService.java
+++ src/main/java/com/example/demo/service/DocStorageService.java
@@ -2,10 +2,14 @@ package com.example.demo.service;
 
 import com.example.demo.model.Doc;
 import com.example.demo.repository.DocRepository;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,9 +19,23 @@ public class DocStorageService {
 	private DocRepository docRepository;
 
 	public Doc saveFile(MultipartFile file) {
+
 		String docname = file.getOriginalFilename();
 		try {
 			Doc doc = new Doc(docname, file.getContentType(), file.getBytes());
+			if (doc.getDocType().contains("application/vnd.openxmlformats-officedocument.wordprocessingml.document")){
+				System.out.println("doc.getDocName = " + doc.getDocName());
+				System.out.println("doc.getDocType() = " + doc.getDocType());
+				System.out.println("doc.toString() = " + doc.toString());
+				System.out.println("doc = " + doc);
+				System.out.println("docname = " + docname);
+				System.out.println("file.getContentType() = " + file.getContentType());
+				System.out.println("file.getOriginalFilename() = " + file.getOriginalFilename());
+				System.out.println("file.getBytes() = " + file.getBytes());
+				System.out.println("file = " + file);
+				;
+				wordToPdf("C:\\ferraz\\anotacoes\\estudos\\Senac\\" + doc.getDocName());
+			}
 			return docRepository.save(doc);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -32,4 +50,26 @@ public class DocStorageService {
 	public List<Doc> getFiles() {
 		return docRepository.findAll();
 	}
+
+	public String wordToPdf(String docname) {
+		System.out.println("docname ============== " + docname);
+			try {
+//				InputStream docFile = new FileInputStream(new File("C:/Users/ferra/Downloads/senac.docx"));
+				InputStream docFile = new FileInputStream(docname);
+				XWPFDocument doc = new XWPFDocument(docFile);
+				PdfOptions pdfOptions = PdfOptions.create();
+				OutputStream out = new FileOutputStream(new File("C:/Users/ferra/Downloads/qqb.pdf"));
+				PdfConverter.getInstance().convert(doc, out, pdfOptions);
+				doc.close();
+				out.close();
+				System.out.println("Done");
+				return out.toString();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		return "Error";
+	}
+
 }
+
