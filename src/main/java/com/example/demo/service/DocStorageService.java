2125c0c7118792b9a330e109feb54db55fa2e54e
renatoFerraz-git <renatoferrazconta02@gmail.com>
2022-05-03 00:00:49 -0300

    correcao

--- src/main/java/com/example/demo/service/DocStorageService.java
+++ src/main/java/com/example/demo/service/DocStorageService.java
@@ -15,55 +15,51 @@ import java.util.Optional;
 
 @Service
 public class DocStorageService {
-	@Autowired
-	private DocRepository docRepository;
+    @Autowired
+    private DocRepository docRepository;
 
-	public Doc saveFile(MultipartFile file) {
+    public Doc saveFile(MultipartFile file) {
 
-		String docname = file.getOriginalFilename();
-		try {
-			Doc doc = new Doc(docname, file.getContentType(), file.getBytes());
-			if (doc.getDocType().contains("application/vnd.openxmlformats-officedocument.wordprocessingml.document")){
-				String item = wordToPdf("C:\\ferraz\\anotacoes\\estudos\\Senac\\" + doc.getDocName());
-				doc.setDocType(item);
-				System.out.println("doc.getDocName() = " + doc.getDocName());
-				System.out.println("doc.getDocType() = " + doc.getDocType());
+        String docname = file.getOriginalFilename();
+        try {
+            Doc doc = new Doc(docname, file.getContentType(), file.getBytes());
+            if (doc.getDocType().contains("application/vnd.openxmlformats-officedocument.wordprocessingml.document")) {
+                String item = wordToPdf(docname);
+            }
+            return docRepository.save(doc);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-			}
-			return docRepository.save(doc);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public Optional<Doc> getFile(Integer fileId) {
+        return docRepository.findById(fileId);
+    }
 
-	public Optional<Doc> getFile(Integer fileId) {
-		return docRepository.findById(fileId);
-	}
+    public List<Doc> getFiles() {
+        return docRepository.findAll();
+    }
 
-	public List<Doc> getFiles() {
-		return docRepository.findAll();
-	}
+    public String wordToPdf(String docname) {
+        System.out.println("docname = " + docname);
+        try {
+            //InputStream docFile = new FileInputStream(new File("C:/Users/ferra/Downloads/senac.docx"));
+            InputStream docFile = new FileInputStream(docname);
+            XWPFDocument doc = new XWPFDocument(docFile);
+            PdfOptions pdfOptions = PdfOptions.create();
+            OutputStream out = new FileOutputStream(new File("C:/Users/ferra/Downloads/qqb.pdf"));
+            PdfConverter.getInstance().convert(doc, out, pdfOptions);
+            doc.close();
+            out.close();
+            System.out.println("Done");
+            return out.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	public String wordToPdf(String docname) {
-		System.out.println("docname ============== " + docname);
-			try {
-//				InputStream docFile = new FileInputStream(new File("C:/Users/ferra/Downloads/senac.docx"));
-				InputStream docFile = new FileInputStream(docname);
-				XWPFDocument doc = new XWPFDocument(docFile);
-				PdfOptions pdfOptions = PdfOptions.create();
-				OutputStream out = new FileOutputStream(new File("C:/Users/ferra/Downloads/qqb.pdf"));
-				PdfConverter.getInstance().convert(doc, out, pdfOptions);
-				doc.close();
-				out.close();
-				System.out.println("Done");
-				return out.toString();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-
-		return "Error";
-	}
+        return "Error";
+    }
 
 }
 
